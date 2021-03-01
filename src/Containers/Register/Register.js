caf8924f3eeb0a32f4569099a329bdd1c9ddb63c
Jainam Mehta <jainamm47@gmail.com>
2021-03-01 20:21:11 +0530

    Changes in Register Page UI

--- src/Containers/Register/Register.js
+++ src/Containers/Register/Register.js
@@ -1,12 +1,12 @@
 /** @format */
 
-import React from "react";
+import React, { Fragment } from "react";
 import classes from "./Register.module.css";
-import RegisterImage from "../../assets/Register.svg";
+import RegisterSVG from "../../assets/Register.svg";
 
 const Register = () => {
   return (
-    <div>
+    <Fragment>
       <div className={classes.background}>
         <div className={classes.Container}>
           <div className={classes.SVGContainer}>
@@ -14,14 +14,14 @@ const Register = () => {
               height='450px'
               width='500px'
               className={classes.svg}
-              src={RegisterImage}
-              alt='register'
+              src={RegisterSVG}
+              alt='register Svg'
             />
           </div>
           <div className={classes.form}>
             <h3 className={classes.heading}>Welcome to Talent Hunt</h3>
-            <div className={classes.flex} style={{ marginTop: "1%" }}>
-              <div className={classes.flexColumn}>
+            <div className={classes.flex}>
+              <div>
                 <label className={classes.label}>Name</label>
                 <br />
                 <input
@@ -31,59 +31,50 @@ const Register = () => {
                 />
               </div>
               <div className={classes.margin}>
-                <div className={classes.flexColumn}>
-                  <label className={classes.label}>Email</label>
-                  <br />
-                  <input
-                    type={"email"}
-                    placeholder={"mark.cuban@gmail.com"}
-                    className={classes.input}
-                  />
-                </div>
+                <label className={classes.label}>Email</label>
+                <br />
+                <input
+                  type={"email"}
+                  placeholder={"mark.cuban@gmail.com"}
+                  className={classes.input}
+                />
               </div>
-              <div className={classes.flex}>
-                <div className={classes.flexColumn}>
-                  <label className={classes.label}>Password</label>
-                  <br />
-                  <input type={"password"} className={classes.input} />
-                </div>
-                <div className={classes.margin}>
-                  <label className={classes.label}>Confirm Password</label>
-                  <br />
-                  <input type={"password"} className={classes.input} />
-                  <input type={"text"} className={classes.input} />
-                </div>
-                <div className={classes.flexColumn}>
-                  <label className={classes.label}>Confirm password</label>
-                  <input type={"email"} className={classes.input} />
-                </div>
+            </div>
+            <div className={classes.flex}>
+              <div>
+                <label className={classes.label}>Password</label>
+                <br />
+                <input type={"password"} className={classes.input} />
               </div>
-              <div
-                className={classes.flex}
-                style={{ justifyContent: "flex-start" }}>
-                <label>Gender</label>
-                <select className={classes.select}>
-                  <option>Male</option>
-                  <option>Female</option>
-                </select>
+              <div className={classes.margin}>
+                <label className={classes.label}>Confirm Password</label>
+                <br />
+                <input type={"password"} className={classes.input} />
               </div>
-              <label className={classes.label}>Bio</label>
-              <textarea className={(classes.input, classes.textarea)} />
-              <div className={classes.flex}>
-                <input type={"checkbox"} className={classes.checkbox} />
-                <label className={classes.terms}>
-                  Yes, I understand the Terms of Service of TalentHunt,
-                  including the User Agreement and Privacy Policy
-                </label>
-              </div>
-              <div className={classes.btnContainer}>
-                <button className={classes.btn}>Register</button>
-              </div>
+            </div>
+            <div className={classes.flex}>
+              <label>Gender</label>
+              <select className={classes.select}>
+                <option>Male</option>
+                <option>Female</option>
+              </select>
+            </div>
+            <label className={classes.label}>Bio</label>
+            <textarea className={(classes.input, classes.textarea)} />
+            <div className={classes.flex}>
+              <input type={"checkbox"} className={classes.checkbox} />
+              <label className={classes.terms}>
+                Yes, I understand the Terms of Service of TalentHunt, including
+                the User Agreement and Privacy Policy
+              </label>
+            </div>
+            <div className={classes.btnContainer}>
+              <button className={classes.btn}>Register</button>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </Fragment>
   );
 };
 
